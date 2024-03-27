45ae3c6c31851ed928b30d2ff180b028ba0100a6
jatin Bhatt <jatin.b.php@gmail.com>
2024-03-27 18:21:08 +0530

    creating view pages

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <header>
+    <div class="wrapper">
+      <nav class="navbar navbar-expand-lg navbar-light bg-dark">
+        <div class="container-fluid">
+            <a class="navbar-brand text-white" href="#">Vue App</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav ms-auto">
+                    <li class="nav-item">
+                        <RouterLink class="nav-link text-white active" aria-current="page" to="#">Home</RouterLink>
+                    </li>
+                    <li class="nav-item">
+                        <RouterLink class="nav-link text-white" to="/students">Students</RouterLink>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+    </div>
+  </header>
+
+  <RouterView />
+</template>
+
---
+++ src/views/student/view.vue
@@ -0,0 +1,62 @@
+<template>
+    <main>
+        <div class="container-fluid">
+            <div class="row mt-2">
+                <div class="col-12">
+                    <div class="card card-info card-outline">
+                        <div class="card-header">
+                            <div class="row">
+                                <div class="col-md-6">
+                                    <h3 class="card-title">Students</h3>
+                                </div>
+                                <div class="col-md-6">
+                                    <RouterLink to="" class="btn btn-sm btn-info float-end"><i class="fa fa-plus pr-1"></i> Add New</RouterLink>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="card-body table-responsive">
+                            <table id="contentTable" class="table table-bordered table-striped datatable-dynamic">
+                                <thead>
+                                    <tr>
+                                        <th>#</th>
+                                        <th>Title</th>
+                                        <th>Action</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </main>
+</template>
+
+<script>
+    import axios from 'axios'
+
+    export default {
+        name: 'students',
+        data(){
+            return {
+                students: []
+            }
+        },
+        mounted(){
+            this.getStudents();
+        },
+        
+        methods: {
+            async getStudents() {
+                try {
+                    const response = await axios.get('your-api-endpoint-url');
+                    console.log(response.data); // Access response data
+                } catch (error) {
+                    console.error('Error fetching data:', error);
+                }
+            }
+        }
+    }
+</script>
